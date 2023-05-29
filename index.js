e5787f5ab01761bf91a88c7effeaee16c0bebe53
RAJ KUMAR SEN <rajkumarshrivash374@gmail.com>
2023-05-29 14:13:01 +0530

    updated

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ const middlewares = jsonServer.defaults()
 server.use(middlewares)
 server.use(router)
 
-server.listen(4000, () => {
+server.listen(process.env.port || 4000, () => {
   console.log('JSON Server is running')
 })
